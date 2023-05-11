6934a91b5de47c6dc9caf5189e99b8101871fa1d
Jett Tipsword <107717668+jett512@users.noreply.github.com>
2023-05-11 23:50:27 +0000

    q

--- google_streetview/Module.py
+++ google_streetview/Module.py
@@ -1,4 +1,5 @@
 # Import google_streetview for the api module
+
 import google_streetview.api
 
 # Define parameters for street view api
@@ -7,11 +8,11 @@ params = [{
   'location': '46.414382,10.013988',
   'heading': '151.78',
   'pitch': '-0.76',
-  'key': 'your_dev_key'
+  'key': 'AIzaSyDCWUCnWO7NmVBcP3FKQGY8iOTkpbXG5BA'
 }]
 
 # Create a results object
 results = google_streetview.api.results(params)
 
 # Download images to directory 'downloads'
-results.download_links('downloads')
+results.download_links('download')
