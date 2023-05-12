cdbe29d8ea50c18aae28b66fb0d3e2ea5e4d98b0
Jett Tipsword <107717668+jett512@users.noreply.github.com>
2023-05-12 14:06:22 +0000

    Pending changes exported from your codespace

--- google_streetview/Module.py
+++ google_streetview/Module.py
@@ -4,11 +4,12 @@ import google_streetview.api
 
 # Define parameters for street view api
 params = [{
-  'size': '600x300', # max 640x640 pixels
-  'location': '46.414382,10.013988',
+  'size': '1920x1080', # max 640x640 pixels
+  'location': '29.84458027616346, -97.96978071621939',
   'heading': '151.78',
-  'pitch': '-0.76',
-  'key': 'AIzaSyDCWUCnWO7NmVBcP3FKQGY8iOTkpbXG5BA'
+  'pitch': '-120',
+  'key': 'AIzaSyBs541YgMqil11RKj6jgQxHxNXUukljyxY',
+  'fov': '120'
 }]
 
 # Create a results object
